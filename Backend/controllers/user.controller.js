941daa02e5a6001e95d136e58a2eb6e1c1ebbdf5
Dhruva <dhruva.joshi@sjsu.edu>
2025-05-12 22:27:26 -0700

    Updated user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+//user controller.jsx
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
