fbe920f8194b9ede98401d8e1d90e3d747002a47
Aayushi2504 <aayushiparag.shah@sjsu.edu>
2025-05-12 22:22:55 -0700

    Updated client.js

--- Backend/redis/client.js
+++ Backend/redis/client.js
@@ -5,6 +5,6 @@ const client = redis.createClient({ url: 'redis://localhost:6379' });
 client.on('error', (err) => console.error('Redis error:', err));
 client.on('connect', () => console.log('Redis client connected'));
 
-client.connect(); // Must call connect() with modern Redis
+client.connect(); 
 
 module.exports = client;
