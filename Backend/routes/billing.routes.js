196e9b659960ffc96285ce490799ae94a633cc4e
Aayushi2504 <aayushiparag.shah@sjsu.edu>
2025-05-12 22:20:50 -0700

    Updated billing.routes.js

--- Backend/routes/billing.routes.js
+++ Backend/routes/billing.routes.js
@@ -3,18 +3,10 @@ const router = express.Router();
 const billingController = require('../controllers/billing.controller');
 const { authUser, authAdmin } = require('../middlewares/auth.middleware');
 
-
-// Admin: get all billing records
 router.get('/', billingController.getAllBillings);
-
-// User: get their own billing records
 router.get('/user/:userId', authUser, billingController.getBillingByUser);
-
-// Get billing by ride ID
 router.get('/ride/:rideId', authUser, billingController.getBillingByRide);
-
 router.get('/by-driver/:driverId', billingController.getBillingByDriver);
-
 router.post('/', authUser, billingController.createBilling);
 router.put('/:id', authUser, billingController.updateBilling);
 router.delete('/:id', authAdmin, billingController.deleteBilling);
